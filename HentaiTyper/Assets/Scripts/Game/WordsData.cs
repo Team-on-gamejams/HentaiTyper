60db0cffdaaca5dcab367ede01c202c334cae356
Team-on <timonsol13@gmail.com>
2019-11-24 03:54:12 +0200

    Parser for manga

--- HentaiTyper/Assets/Scripts/Game/WordsData.cs
+++ HentaiTyper/Assets/Scripts/Game/WordsData.cs
@@ -2,14 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "New WordsData", menuName = "Words Data", order = 51)]
 public class WordsData : ScriptableObject {
 	public List<Word> words;
+
+	public void ParseWordsData() {
+		words = new List<Word>();
+		string[] guids = AssetDatabase.FindAssets("t:sprite", new[] { "Assets/Sprites/Manga" });
+		foreach (var guid in guids) {
+			Sprite art = AssetDatabase.LoadAssetAtPath<Sprite>(AssetDatabase.GUIDToAssetPath(guid));
+
+			words.Add(new Word {
+				word = art.name,
+				images = new List<Sprite>() { art }
+			});
+		}
+	}
 }
 
 [Serializable]
 public class Word {
 	public string word;
 	public List<Sprite> images;
+}
+
+[CustomEditor(typeof(WordsData))]
+public class WordsDataEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		WordsData data = (WordsData)target;
+		if (GUILayout.Button("Parse Sprites/Manga folder")) {
+			data.ParseWordsData();
+		}
+	}
 }
